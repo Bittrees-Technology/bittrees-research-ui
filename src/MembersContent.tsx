3dd7b6e94f8191c358f2f6d30b37362dc67712fb
David Tam <david.tam@walgreens.com>
2023-04-01 19:27:49 -0500

    keeps returning false

--- src/MembersContent.tsx
+++ src/MembersContent.tsx
@@ -4,62 +4,58 @@ import { goerli, mainnet } from "wagmi/chains";
 import { ethers } from "ethers";
 import { useState } from "react";
 
-import { Network, Alchemy } from "alchemy-sdk";
+//import { Network, Alchemy } from "alchemy-sdk";
 
-const alchemyConfig = {
-  apiKey: "MY6sRxkJ6Jeo6Pd_6XvgrmvXJFbrQE0w",
-  network: Network.ETH_MAINNET,
-};
+//const alchemyConfig = {
+//  apiKey: "MY6sRxkJ6Jeo6Pd_6XvgrmvXJFbrQE0w",
+//  network: Network.ETH_MAINNET,
+//};
 
-const alchemy = new Alchemy(alchemyConfig);
+//const alchemy = new Alchemy(alchemyConfig);
 
 const CONTRACT_ADDRESS = "0xc8121e650bd797d8b9dad00227a9a77ef603a84a";
 const chainId =
   process.env.REACT_APP_ENABLE_TESTNETS === "true" ? goerli.id : mainnet.id;
 
-console.info(`Contract: ${CONTRACT_ADDRESS}`);
-console.info(`Chain ID: ${chainId}`);
+//console.info(`Contract: ${CONTRACT_ADDRESS}`);
+//console.info(`Chain ID: ${chainId}`);
 
-const mintPrice = "0.0250";
-
-function displayFriendlyError(message: string | undefined): string {
-  if (!message) return "";
-
-  if (
-    message.startsWith("insufficient funds for intrinsic transaction cost")
-  ) {
-    return "insufficient funds for intrinsic transaction cost.";
-  }
-  return message;
-}
 
 export function MembersContent() {
-  const [total, setTotal] = useState(mintPrice);
-
-  function calcTotal(donation: string) {
-    const total = (
-      parseFloat(mintPrice) + parseFloat(donation ? donation : "0")
-    ).toFixed(4);
-    setTotal(total);
-  
-
-  }
-
+ 
   const { address } = useAccount();
+  console.info(`address: ${address}`);
 
-  const { config, error } = usePrepareContractWrite({
-    address: CONTRACT_ADDRESS,
-    abi,
-    functionName: "mintMembership",
-    chainId: chainId,
-    args: [address],
-    overrides: { value: ethers.utils.parseEther(total) },
-  });
-  const { isLoading, write } = useContractWrite(config);
+  function lookForNFTs() {
 
-  function onClick() {
-    write?.();
+    fetch(`https://eth-mainnet.g.alchemy.com/v2/g6X4-HRGshx5XNp7gpDxLPeX-WSpw9pN/getNFTs/?owner=0x7435e7f3e6B5c656c33889a3d5EaFE1e17C033CD`)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        //console.log(result);
+        if (result.ownedNfts.length == 0) {
+          console.log("No NFTs.")
+        } else {
+          for(let i = 0; i < result.ownedNfts.length; i++) {
+            let nftInfo = result.ownedNfts[i];
+            console.log("contract address: " + nftInfo.contract.address);
+            if(nftInfo.contract.address === CONTRACT_ADDRESS) {
+              console.info("Bittrees Research contract found!!!")
+              return true;
+            } else {
+              console.info("Not a Bittrees Research contract.")
+            }
+          }
+
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+    return false;
   }
+
 
 
     // alchemy.nft.getNftsForOwner({ssx.address()}`)
@@ -72,47 +68,32 @@ export function MembersContent() {
 
   return (
     <>
-      <div className="grid grid-cols-2 gap-6 justify-start">
-        <div className="text-right">Minimum Donation:</div>
-        <div className="text-left">{mintPrice} ETH</div>
-        <div className="text-right">Add additional donation:</div>
-        <div className="text-left">
-          <input
-            className="w-20 input-sm"
-            type="number"
-            placeholder="0.00"
-            onChange={(e) => calcTotal(e.target.value)}
-            step="0.01"
-            min="0"
-          />{" "}
-          <span>ETH</span>
-        </div>
-        <div className="text-right">Total price:</div>
-        <div className="text-left">
-          {total} <span>ETH</span>
-        </div>
-      </div>
-      {error && (
-        <div className="m-4 mx-auto max-w-xl">
-          An error occurred preparing the transaction:{" "}
-          {displayFriendlyError(error.message)}
-        </div>
-      )}
+
+      {!address && (
+          <p className="text-2xl mt-4">Please connect your wallet.</p>
+       )}
+
 
       <div className="mt-4">
+
+
         <button
           className="btn btn-primary"
-          onClick={onClick}
-          disabled={!Boolean(address) || Boolean(error)}
+          onClick={lookForNFTs}
         >
-          Mint
+          Authenticate
         </button>
 
-        {!address && (
-          <p className="text-2xl mt-4">Please connect your wallet.</p>
+
+        {lookForNFTs() && (
+          <div className="m-4 mx-auto max-w-xl">
+            Members Content!!! For some reason it keeps returning false.
+          </div>
         )}
-        {isLoading && <p className="text-2xl mt-4">Minting...</p>}
+
       </div>
+
+
     </>
   );
 }
