0713f05a2d181f25914394f587d1901ef41af157
Brian Gershon <briangershon@users.noreply.github.com>
2023-04-02 11:07:08 -0700

    Refactor membership logic. Add helper functions.
    Remove debugging info.

--- src/MembersContent.tsx
+++ src/MembersContent.tsx
@@ -1,107 +1,102 @@
-import { useAccount, useContract, useContractRead } from "wagmi";
+import { useAccount } from "wagmi";
 import abi from "./abi.json";
 import { goerli, mainnet } from "wagmi/chains";
 import { ethers } from "ethers";
 import { useState, useEffect } from "react";
 
 const CONTRACT_ADDRESS = "0xc8121e650bd797d8b9dad00227a9a77ef603a84a";
+const RPC_URL =
+  "https://eth-mainnet.g.alchemy.com/v2/g6X4-HRGshx5XNp7gpDxLPeX-WSpw9pN";
 const chainId =
   process.env.REACT_APP_ENABLE_TESTNETS === "true" ? goerli.id : mainnet.id;
 
 //console.info(`Contract: ${CONTRACT_ADDRESS}`);
 //console.info(`Chain ID: ${chainId}`);
 
+interface OwnedNFT {
+  contract: {
+    address: string;
+  };
+  id: {
+    tokenId: string;
+  };
+}
+
+/**
+ *
+ * @param tokenId - hex string such as 0x01
+ * @returns boolean
+ */
+async function isMembershipExpired(tokenId: string): Promise<boolean> {
+  const provider = await new ethers.providers.JsonRpcProvider(RPC_URL);
+  const contract = await new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
+  return contract.isExpired(tokenId);
+}
+
+/**
+ *
+ * @param ownerAddress - wallet address
+ * @param contractAddress
+ * @returns string[] - array of tokenIds
+ */
+async function getMemberTokenIds(
+  ownerAddress: string,
+  contractAddress: string
+): Promise<string[]> {
+  const alchemyEndpoint = `${RPC_URL}/getNFTs/?owner=${ownerAddress}`;
+  const response = await fetch(alchemyEndpoint);
+  const data = await response.json();
+  const ownedNfts = data.ownedNfts as OwnedNFT[];
+  const contractNfts = ownedNfts.filter((nft) => {
+    return nft.contract.address === contractAddress;
+  });
+  return contractNfts.map((nft) => {
+    return nft.id.tokenId;
+  });
+}
+
+/**
+ *
+ * @param ownerAddress
+ * @returns boolean
+ */
+async function hasActiveMembership(ownerAddress: string): Promise<boolean> {
+  const tokenIds = await getMemberTokenIds(ownerAddress, CONTRACT_ADDRESS);
+  const isExpired = await Promise.all(
+    tokenIds.map((tokenId) => {
+      return isMembershipExpired(tokenId);
+    })
+  );
+  const hasActiveMembership = isExpired.some((isExpired) => {
+    return !isExpired;
+  });
+  return hasActiveMembership;
+}
+
 export function MembersContent() {
-  const [nftData, setNftData] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [isNFTOwner, setNFTOwner] = useState(false);
- 
+  const [hasValidMembership, setHasValidMembership] = useState(false);
+
   const { address } = useAccount();
-  console.info(`address: ${address}`);
-
-  const contract = useContract({
-    address: CONTRACT_ADDRESS,
-    abi: abi,
-  })
-
-  console.log(contract);
-
-  // testing - results in error, should return boolean, there is a tokenId of 0x01
-  const contractRead = useContractRead({
-    address: CONTRACT_ADDRESS,
-    abi: abi,
-    functionName: 'isExpired',
-    chainId: chainId,
-    args: [ 1 ],
-    onSettled(data, error) {
-      console.log('Settled', { data, error })
-    },
-  })
-
-  // if (!isError) {
-  //console.log("data: "+ JSON.stringify(data));
-  // }
-
-
-  //0x01
-  //console.log(JSON.stringify(isError));
-
-  // const contractAddress = '0xc8121e650bd797d8b9dad00227a9a77ef603a84a'
-  // const provider =  new ethers.providers.JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/g6X4-HRGshx5XNp7gpDxLPeX-WSpw9pN");
-  // const contract =  new ethers.Contract( contractAddress , abi , provider );
-  // const expired =  contract.isExpired('1');
-
-  
 
   useEffect(() => {
     if (!address) {
-      setNFTOwner(false);
+      setHasValidMembership(false);
       return;
     }
-    let alchemyEndpoint = "https://eth-mainnet.g.alchemy.com/v2/g6X4-HRGshx5XNp7gpDxLPeX-WSpw9pN/getNFTs/?owner=" + address;
- 
-    // TESTING... Hardcoded the address 0x7435e7f3e6B5c656c33889a3d5EaFE1e17C033CD. 
-    //alchemyEndpoint = "https://eth-mainnet.g.alchemy.com/v2/g6X4-HRGshx5XNp7gpDxLPeX-WSpw9pN/getNFTs/?owner=0x7435e7f3e6B5c656c33889a3d5EaFE1e17C033CD";
-    console.log("alchemyEndpoint: " + alchemyEndpoint);
-  
-    fetch( alchemyEndpoint )
-      .then(response => response.json())
-      .then((usefulData) => {
-        if (usefulData.ownedNfts.length == 0) {
-          console.log("No NFTs.")
-        } else {
-          for(let i = 0; i < usefulData.ownedNfts.length; i++) {
-            let nftInfo = usefulData.ownedNfts[i];
-            console.log("contract address: " + nftInfo.contract.address);
-            if(nftInfo.contract.address === CONTRACT_ADDRESS) {
-              console.info("Bittrees Research contract found!!!")
-              console.log(nftInfo.id.tokenId);
-              console.log(nftInfo.id.tokenMetadata);
-              console.log(nftInfo.title);
-              setNFTOwner(true);
-              setLoading(false);
-              setNftData(nftInfo);
-              console.log(nftData);
-              break;
-            } else {
-              console.info("Not a Bittrees Research contract.")
-            }
-          }
 
-        }
-        // console.log("usefulData: " + usefulData);
-        // console.log(usefulData);
-        // setLoading(false);
-        // setData(usefulData);
+    hasActiveMembership(address)
+      .then((hasActiveMembership) => {
+        setHasValidMembership(hasActiveMembership);
       })
-      .catch((e) => {
-        console.error(`An error occurred: ${e}`)
-        setNFTOwner(false);
+      .catch((err) => {
+        console.error(err);
+        setHasValidMembership(false);
+      })
+      .finally(() => {
+        setLoading(false);
       });
-
   }, [address]);
-
 
   return (
     <>
@@ -109,42 +104,31 @@ export function MembersContent() {
         {!address && (
           <p className="text-2xl mt-4">Please connect your wallet.</p>
         )}
-        {loading && isNFTOwner && <p className="text-2xl mt-4">Loading...</p>}
+        {loading && hasValidMembership && (
+          <p className="text-2xl mt-4">Loading...</p>
+        )}
       </div>
 
+      {hasValidMembership && (
+        <div className="m-4 mx-auto max-w-xl">
+          Members Content!!!
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      )}
 
-
-      {isNFTOwner && (
-          <div className="m-4 mx-auto max-w-xl">
-            Members Content!!!
-
-            <br/><br/>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-            <br/>
-            {chainId}
-            <br/>
-            {CONTRACT_ADDRESS}
-            <br/>
-            {JSON.stringify(nftData)}
-
-
-
-
-          </div>
-        )}
-
-      {!isNFTOwner && (
-          <div className="m-4 mx-auto max-w-xl">
-            Please go mint your membership!
-
-
-
-
-          </div>
-        )}
-
-
+      {!hasValidMembership && (
+        <div className="m-4 mx-auto max-w-xl">
+          Please go mint your membership!
+        </div>
+      )}
     </>
   );
 }
--- src/MembersPage.tsx
+++ src/MembersPage.tsx
@@ -37,34 +37,6 @@ function MembersPage() {
             <MembersContent />
           </div>
 
-          <div className="space-y-5 mt-8">
-            <div className="md:px-8">
-              <hr className="border-gray-500" />
-            </div>
-            <p className="md:px-12 text-justify">
-              Bittrees Research is an organization focused on promoting research
-              in emerging technologies, systems innovation, and related fields
-              with a goal of creating new knowledge and tools whilst fostering
-              innovation with a positive impact in the metaverse and beyond.
-            </p>
-            <p className="md:px-12 text-justify">
-              By studying a wide range of topics, Bittrees Research generates
-              insights that can inform policy, strategy, and decision-making
-              processes, helping to advance society towards a better future.
-            </p>
-            <p className="md:px-12 text-justify">
-              Bittrees Research places a strong emphasis on historical and
-              contextual relevance, recognizing the importance of understanding
-              past successes and failures in order to develop effective
-              solutions for a more just and equitable society. Join the
-              conversation on how emerging technologies and systems innovation
-              can help create a more equitable and sustainable world.
-            </p>
-            <p className="md:px-12 text-justify">
-              Memberships are subject to an expiration 360 days from the date of
-              mint.
-            </p>
-          </div>
           <footer className="flex flex-col gap-6 mx-auto mt-4">
             <div className="underline">
               <a
