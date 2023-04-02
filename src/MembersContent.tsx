24cb74727bb65f06c79f7018c605efd5a396030a
David Tam <david.tam@walgreens.com>
2023-04-01 20:37:32 -0500

    works kinda

--- src/MembersContent.tsx
+++ src/MembersContent.tsx
@@ -2,7 +2,7 @@ import { useAccount, usePrepareContractWrite, useContractWrite } from "wagmi";
 import abi from "./abi.json";
 import { goerli, mainnet } from "wagmi/chains";
 import { ethers } from "ethers";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 //import { Network, Alchemy } from "alchemy-sdk";
 
@@ -22,40 +22,13 @@ const chainId =
 
 
 export function MembersContent() {
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
  
   const { address } = useAccount();
   console.info(`address: ${address}`);
-
-  function lookForNFTs() {
-
-    fetch(`https://eth-mainnet.g.alchemy.com/v2/g6X4-HRGshx5XNp7gpDxLPeX-WSpw9pN/getNFTs/?owner=0x7435e7f3e6B5c656c33889a3d5EaFE1e17C033CD`)
-    .then(res => res.json())
-    .then(
-      (result) => {
-        //console.log(result);
-        if (result.ownedNfts.length == 0) {
-          console.log("No NFTs.")
-        } else {
-          for(let i = 0; i < result.ownedNfts.length; i++) {
-            let nftInfo = result.ownedNfts[i];
-            console.log("contract address: " + nftInfo.contract.address);
-            if(nftInfo.contract.address === CONTRACT_ADDRESS) {
-              console.info("Bittrees Research contract found!!!")
-              return true;
-            } else {
-              console.info("Not a Bittrees Research contract.")
-            }
-          }
-
-        }
-      },
-      (error) => {
-        console.log(error);
-      }
-    )
-    return false;
-  }
-
 
 
     // alchemy.nft.getNftsForOwner({ssx.address()}`)
@@ -66,32 +39,77 @@ export function MembersContent() {
     //   };
 
 
+  // Hardcoded the address. Trying to get the address into the fetch url. 
+
+  useEffect(() => {
+    fetch("https://eth-mainnet.g.alchemy.com/v2/g6X4-HRGshx5XNp7gpDxLPeX-WSpw9pN/getNFTs/?owner=0x7435e7f3e6B5c656c33889a3d5EaFE1e17C033CD")
+      .then(response => response.json())
+      .then((usefulData) => {
+        if (usefulData.ownedNfts.length == 0) {
+          console.log("No NFTs.")
+        } else {
+          for(let i = 0; i < usefulData.ownedNfts.length; i++) {
+            let nftInfo = usefulData.ownedNfts[i];
+            console.log("contract address: " + nftInfo.contract.address);
+            if(nftInfo.contract.address === CONTRACT_ADDRESS) {
+              console.info("Bittrees Research contract found!!!")
+              console.log(nftInfo.id.tokenId);
+              console.log(nftInfo.id.tokenMetadata);
+              console.log(nftInfo.title);
+              setLoading(false);
+              setData(nftInfo);
+              console.log(data);
+              break;
+            } else {
+              console.info("Not a Bittrees Research contract.")
+            }
+          }
+
+        }
+        // console.log("usefulData: " + usefulData);
+        // console.log(usefulData);
+        // setLoading(false);
+        // setData(usefulData);
+      })
+      .catch((e) => {
+        console.error(`An error occurred: ${e}`)
+      });
+
+  }, []);
+
+
   return (
     <>
-
-      {!address && (
-          <p className="text-2xl mt-4">Please connect your wallet.</p>
-       )}
-
-
       <div className="mt-4">
+        {!address && (
+          <p className="text-2xl mt-4">Please connect your wallet.</p>
+        )}
+        {loading && <p className="text-2xl mt-4">Loading...</p>}
+      </div>
 
 
-        <button
-          className="btn btn-primary"
-          onClick={lookForNFTs}
-        >
-          Authenticate
-        </button>
-
-
-        {lookForNFTs() && (
+      {data && (
           <div className="m-4 mx-auto max-w-xl">
-            Members Content!!! For some reason it keeps returning false.
+            Members Content!!!
+
+            <br/><br/>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+            <br/>
+            {chainId}
+            <br/>
+            {CONTRACT_ADDRESS}
+            <br/>
+            { 
+
+              
+            }
+
+
+
+
           </div>
         )}
-
-      </div>
 
 
     </>
