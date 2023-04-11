e2ea4ef611b180e6934aa2d97660a6820ee9fe31
David Tam <david.tam@walgreens.com>
2023-04-10 21:29:06 -0500

    more content

--- src/RoadmapContent.tsx
+++ src/RoadmapContent.tsx
@@ -23,9 +23,14 @@ interface OwnedNFT {
  * @returns boolean
  */
 async function isMembershipExpired(tokenId: string): Promise<boolean> {
-  const provider = await new ethers.providers.JsonRpcProvider(RPC_URL);
-  const contract = await new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
-  return contract.isExpired(tokenId);
+  try {
+    const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
+    return await contract.isExpired(tokenId);
+  } catch (err) {
+    console.error("failure when calling contract.isExpired() due to", err);
+  }
+  return false;
 }
 
 /**
@@ -59,28 +64,35 @@ async function getMemberTokenIds(
  */
 async function hasActiveMembership(ownerAddress: string): Promise<boolean> {
   const tokenIds = await getMemberTokenIds(ownerAddress, CONTRACT_ADDRESS);
+  console.log("tokenId: " + tokenIds);
   const isExpired = await Promise.all(
     tokenIds.map((tokenId) => {
       return isMembershipExpired(tokenId);
     })
   );
+  console.log("isExpired: " + isExpired);
   return isExpired.some((isExpired) => {
     return !isExpired;
   });
 }
 
 /**
- * MembersContent React component.
+ * RoadmapContent React component.
  *
  */
 export function RoadmapContent() {
   const [loading, setLoading] = useState(true);
   const [hasValidMembership, setHasValidMembership] = useState(false);
 
-  const { address } = useAccount();
+  const { address, isConnected, isConnecting } = useAccount({
+    onConnect({ address, connector, isReconnected }) {
+      console.log("Connected", { address, connector, isReconnected });
+    },
+  });
 
   useEffect(() => {
-    if (!address) {
+    console.log("isConnected: " + isConnected);
+    if (!(address && isConnected)) {
       setHasValidMembership(false);
       return;
     }
@@ -96,7 +108,7 @@ export function RoadmapContent() {
       .finally(() => {
         setLoading(false);
       });
-  }, [address]);
+  }, [address, isConnected]);
 
   return (
     <>
@@ -110,15 +122,18 @@ export function RoadmapContent() {
       </div>
 
       {hasValidMembership && (
-        <div className="m-4 mx-auto max-w-xl">
-          
-          <ul>
-            <li>Organization: BR</li>
-            <ul>
+        <div className="m-4 mx-auto max-w-xl text-left">
+          <div className="inline-flex items-center justify-center w-full">
+              <hr className="w-full h-px my-8 bg-gray-500 border-0 dark:bg-gray-700" />
+              <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">Bittrees Research [BR]</span>
+          </div>
+          <ul className="list-disc list-outside text-gray-600 font-light">
+            <li><b className="font-semibold">Organization: BR</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>Roadmap</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>Alpha</li>
-                <ul>
+                <ul className="list-disc list-outside pl-4">
                   <li>Main Landing Page</li>
                   <li>Membership mint page [term membership framework]</li>
                   <li>Assign governors to escrow</li>
@@ -128,7 +143,7 @@ export function RoadmapContent() {
                   <li>Gitbook</li>
                 </ul>
                 <li>Beta</li>
-                <ul>
+                <ul className="list-disc list-outside pl-4">
                   <li>Member Services landing page</li>
                   <li>Offer Equity NFTs to members</li>
                   <li>Offer Trust deployer as LOI</li>
@@ -137,7 +152,7 @@ export function RoadmapContent() {
                   <li>Assign Executive members</li>
                 </ul>
                 <li>Launch</li>
-                <ul>
+                <ul className="list-disc list-outside pl-4">
                   <li>Budget Proposal</li>
                   <li>Deploy Public Treasury</li>
                   <li>Redirect  membership fees into BR Public Treasury</li>
@@ -148,28 +163,30 @@ export function RoadmapContent() {
                   <li>Allow Burn/Swap function for the Public Treasury</li>
                 </ul>
                 <li>Growth</li>
-                <ul>
+                <ul className="list-disc list-outside pl-4">
                   <li>Asset and debt deposits/withdrawals</li>
                 </ul>    
               </ul>
             </ul>
           </ul>
 
-          <hr/>
-          Alpha
-          <hr/>
-          <ul>
-            <li>Landing page</li>
-            <ul>
+          <div className="inline-flex items-center justify-center w-full">
+              <hr className="w-full h-px my-8 bg-gray-500 border-0 dark:bg-gray-700" />
+              <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">Alpha</span>
+          </div>
+
+          <ul className="list-disc list-outside text-gray-600 font-light">
+            <li><b className="font-semibold">Landing page</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>Portal to membership minting page</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>Can become a member</li>
                 <li>Can additional donation</li>
               </ul>
               <li>Portal to members services</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>Link to Governance</li>
-                  <ul>
+                <ul className="list-disc list-outside pl-4">
                     <li>At initiation use snapshot</li>
                   </ul>
                   <li>Link to Virtual Lodge</li>
@@ -178,31 +195,31 @@ export function RoadmapContent() {
               </ul>
             </ul>
 
-            <li>Membership Mint page</li>
-            <ul>
+            <li><b className="font-semibold">Membership Mint page</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>Membership NFTs are deployed to an address and while that remains the address is viewed as a Member</li>
               <li>Wallet login</li>
               <li>Mint button and cost of 0.025 eth</li>
               <li>Add additional donation</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>This implies the mint of the membership is granted upon receiving 0.025 ether</li>
               </ul>
               <li>Brief description of the organization</li>
               <li>Disclaimer to be attached to each membership</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>The BR Membership NFT grants a membership token gated access to the BR lounge and other services; memberships are valid for a term of 360 days from the time of mint.</li>
               </ul>
               <li>No limit to allowed memberships or donations</li>
               <li>Free mint can be assigned to individuals or lists</li>
             </ul>
 
-            <li>Governors</li>
-            <ul>
+            <li><b className="font-semibold">Governors</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>Assigned by the BR at initiation</li>
               <li>BR has authority to reassign or delegate authority to members</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>Governors are the acting trustees for the organization</li>
-                  <ul>
+                  <ul className="list-disc list-outside pl-4">
                     <li>At initiation the governor is BMS</li>
                     <li>This will transition to a council </li>
                     <ul>
@@ -211,37 +228,37 @@ export function RoadmapContent() {
                   </ul>
               </ul>
               <li>Governors can update the following attributes for memberships: </li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>price, term of deployed memberships; revoke deployed memberships; assign authority to members.</li>
                 <li>Governors hold the right to revoke a membership given certain criteria is met</li>
               </ul>
             </ul>
 
-            <li>Escrow: this is the cash account assigned to the BR; management of the escrow has been delegated to the governors</li>
-            <ul>
+            <li><b className="font-semibold">Escrow</b>: this is the cash account assigned to the BR; management of the escrow has been delegated to the governors</li>
+            <ul className="list-disc list-outside pl-4">
               <li>The governors will delegate authority to a treasurer </li>
               <li>The will be represented as a gnosis safe</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>This could be replaced with our own custom contract and interface</li>
-                  <ul>
+                  <ul className="list-disc list-outside pl-4">
                     <li>This could be the base for the “trust deployer”</li>
                   </ul>
                 <li>The following addresses will be the delegated signers</li>  
-                  <ul>
+                  <ul className="list-disc list-outside pl-4">
                     <li>The max limit of council members is 10</li>
                   </ul>
               </ul>
             </ul>
 
-            <li>Member</li>
-            <ul>
+            <li><b className="font-semibold">Member</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>Members are the beneficiaries of the BR </li>
               <li>Members hold the BR membership NFT</li>
               <li>BR members are granted token gated access:</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>These are some of the current (alpha) along with future (beta) beneficiary service offers</li>
                 <li>Visit the virtual lodge</li>
-                  <ul>
+                  <ul className="list-disc list-outside pl-4">
                     <li>Meet other members</li>
                     <li>Make budget proposals</li>
                     <li>Make governance proposals</li>
@@ -253,51 +270,55 @@ export function RoadmapContent() {
             </ul>
           </ul>
 
-          <hr/>
-          Beta
-          <hr/>
-          <ul>
-            <li>Equity NFT (preferred stock/share)</li>
-            <ul>
+          <div className="inline-flex items-center justify-center w-full">
+              <hr className="w-full h-px my-8 bg-gray-500 border-0 dark:bg-gray-700" />
+              <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">Beta</span>
+          </div>
+
+          <ul className="list-disc list-outside text-gray-600 font-light">
+            <li><b className="font-semibold">Equity NFT (preferred stock/share)</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>These are offered to members at 1000 bits per share</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>1 bit = 100 sats</li>
                 <li>Proceeds from sales will be escrowed in gnosis and managed by assigned governors or executive members</li>
               </ul>
             </ul>
 
-            <li>BAG template framework</li>
-            <ul>
+            <li><b className="font-semibold">BAG template framework</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>This is be the deployment framework for this type of entity</li>
               <li>This will offer the ability to escrow tokens, assign governor, and offer memberships as a primitive</li>
             </ul>
 
-            <li>Representational Members</li>
-            <ul>
+            <li><b className="font-semibold">Representational Members</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>These are members who have been assigned authority to speak on behalf of other members</li>
             </ul>
 
-            <li>Executive Members</li>
-            <ul>
+            <li><b className="font-semibold">Executive Members</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>These are members who have been assigned duties by the governing body</li>
             </ul>
           </ul>
-           
-          <hr/>
-          Launch
-          <hr/>
-          <ul>
-            <li>Budget Proposal</li>
-            <ul>
+
+          <div className="inline-flex items-center justify-center w-full">
+              <hr className="w-full h-px my-8 bg-gray-500 border-0 dark:bg-gray-700" />
+              <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">Launch</span>
+          </div>
+
+          <ul className="list-disc list-outside text-gray-600 font-light">
+            <li><b className="font-semibold">Budget Proposal</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>This is the budget constructor and proposal system</li>
               <li>This sets the approval process for the budget</li>
               <li>Funding:</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>Hook escrow into bud</li>
                 <li>Hook PS into budget funding</li>
               </ul>
               <li>Procedure: </li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>The governors or executive members propose the budget </li>
                 <li>A % of PS holders need to approve the budget</li>
                 <li>A % of Representative Members need to approve the budget</li>
@@ -305,22 +326,22 @@ export function RoadmapContent() {
               </ul>
             </ul>
 
-            <li>Treasury constructor and NFT claim deloyer</li>
-            <ul>
+            <li><b className="font-semibold">Treasury constructor and NFT claim deloyer</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>Treasury constructor</li>
-                <ul>
+                <ul className="list-disc list-outside pl-4">
                   <li>Treasury Claims [NFTs]</li>
                   <li>The distribution of TC</li>
-                  <ul>
+                  <ul className="list-disc list-outside pl-4">
                     <li>Assigned</li>
                     <li>Burn/swap w/ PS</li>
                   </ul>  
                 </ul>  
               <li>NFT deployer for claims on the BRAG treasury</li>
-                <ul>
+                <ul className="list-disc list-outside pl-4">
                   <li>Holders of burn their PS NFTS and mint NFT Claims</li>
                   <li>Holders of NFT claims can burn/swap, stake, or sell</li>
-                  <ul>
+                  <ul className="list-disc list-outside pl-4">
                     <li>burn/swap their claims at face value of related quantity of treasury reserves</li>
                     <li>Sell on secondary market or over the counter [otc]</li>
                   </ul>  
@@ -328,17 +349,19 @@ export function RoadmapContent() {
             </ul>
           </ul>
 
-          <hr/>
-          Growth
-          <hr/>
-          <ul>
-            <li>Asset and debt deposits</li>
-            <ul>
+          <div className="inline-flex items-center justify-center w-full">
+              <hr className="w-full h-px my-8 bg-gray-500 border-0 dark:bg-gray-700" />
+              <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">Growth</span>
+          </div>
+
+          <ul className="list-disc list-outside text-gray-600 font-light">
+            <li><b className="font-semibold">Asset and debt deposits</b></li>
+            <ul className="list-disc list-outside pl-4">
               <li>This is a public lending utility framework similar to aave or compound</li>
               <li>Stake their claim and get assigned an allotment of fungible credit tokens</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>Credit tokens can be minted at a variable rate by account holder</li>
-                <ul>
+                <ul className="list-disc list-outside pl-4">
                   <li>Credit tokens are ‘bits’</li>
                   <li>1 bit = 100 satoshis</li>
                 </ul>
@@ -346,13 +369,13 @@ export function RoadmapContent() {
 
               <li>Utilize the public lending protocol framework similar to aave or compound</li>
               <li>Allow for the following assets to be deposited:</li>
-              <ul>
+              <ul className="list-disc list-outside pl-4">
                 <li>bit</li>
-                <li>&nbsp;</li>
+                <li>TBA</li>
               </ul>
               <li>Allow for the following tokens to be minted:</li>
-              <ul>
-                <li>&nbsp;</li>
+              <ul className="list-disc list-outside pl-4">
+                <li>TBA</li>
               </ul>
             </ul>
           </ul>
