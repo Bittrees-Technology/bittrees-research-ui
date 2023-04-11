2407ff86f7374d0f24a3902a1fbfdeb5b287e16d
David Tam <david.tam@walgreens.com>
2023-04-10 21:47:47 -0500

    updated content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-  ], 
+  ],
   theme: {
+    fontFamily: {
+      kalam: ["Kalam", "cursive"],
+    },
     extend: {},
-  },
+  }, 
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["cupcake"],
