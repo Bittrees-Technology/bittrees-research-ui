6808617070280433fabc3a851c61535c6f1d8784
David Tam <david.tam@walgreens.com>
2023-04-12 02:22:59 -0500

    moved member services to footer

--- src/MintPage.tsx
+++ src/MintPage.tsx
@@ -63,15 +63,21 @@ function MintPage() {
             </p>
           </div>
           <footer className="flex flex-col gap-6 mx-auto mt-4">
-            <div className="underline">
-              <a
-                href="https://twitter.com/BittreesR"
-                target="_blank"
-                rel="noreferrer"
-              >
-                @BittreesR
+            <span>
+              <a className="hover:text-green-700" href="https://twitter.com/BittreesR" target="_blank" rel="noreferrer">
+                <span className="inline-block align-middle pr-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 hover:text-green-700"
+                  fill="currentColor"
+                  viewBox="0 0 24 24">
+                  <path
+                    d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+                </svg>
+                </span>
+                <span className="inline-block align-middle underline font-newtimesroman pr-1">BittreesR</span>
               </a>
-            </div>
+            </span>
             <div>
               <a href="/" className="mx-auto">
                 <img
@@ -79,7 +85,7 @@ function MintPage() {
                   width="128px"
                   height="128px"
                   alt="Bittrees Research logo"
-                  className="grayscale"
+                  className="grayscale max-w-xs transition duration-300 ease-in-out hover:scale-110"
                 />
               </a>
             </div>
--- src/RoadmapPage.tsx
+++ src/RoadmapPage.tsx
@@ -25,24 +25,23 @@ function RoadmapPage() {
             <ConnectButton />
           </div>
 
+          <div className="mt-4">
+            <RoadmapContent />
+          </div>
+
+          <footer className="flex flex-col gap-6 mx-auto mt-4">
           <span>
             <a className="hover:text-green-700" href="/members">
               <span className="inline-block align-middle pr-1"><svg className="h-4 w-4 hover:text-green-700"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" />  <polyline points="12 8 8 12 12 16" />  <line x1="16" y1="12" x2="8" y2="12" /></svg></span>
               <span className="inline-block align-middle underline font-bold font-newtimesroman">Member Services</span>
             </a>
           </span>
-
-          <div className="mt-4">
-            <RoadmapContent />
-          </div>
-
-          <footer className="flex flex-col gap-6 mx-auto mt-4">
             <span>
               <a className="hover:text-green-700" href="https://twitter.com/BittreesR" target="_blank" rel="noreferrer">
                 <span className="inline-block align-middle pr-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 hover:text-green-700"
+                  className="h-4 w-4 hover:text-green-700"
                   fill="currentColor"
                   viewBox="0 0 24 24">
                   <path
