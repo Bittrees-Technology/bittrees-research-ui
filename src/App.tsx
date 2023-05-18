a05713f263a27509cf239b929c45093ee2fa31eb
David Tam <david.tam@walgreens.com>
2023-05-17 23:17:00 -0500

    landing page changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,7 @@ import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="max-w-4xl mx-auto">
-      <header className="bg-[#dedede] border-b-2 border-gray-500">
-        <div className="w-full">
-          <img
-            className="mx-auto"
-            src="/br-banner.jpg"
-            width="75%"
-            height="75%"
-            alt="Bittrees Research banner"
-          />
-        </div>
-      </header>
-
+      
       <main className="text-center bg-[#dedede]">
         <div className="flex flex-col gap-6 p-6">
           <div className="mx-auto border border-black p-4 w-80 h-64 shadow-md shadow-gray-500 hover:shadow-black">
@@ -44,53 +33,6 @@ function App() {
             </Link>
           </div>
 
-          <footer className="flex flex-col md:flex-row gap-6 mx-auto mt-8 items-center justify-around">
-            <div className="underline w-60">
-              <a
-                href="https://twitter.com/BittreesR"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  className="mx-auto"
-                  src="/twitter.svg"
-                  width="100"
-                  height="100"
-                  alt="Bittrees Research logo"
-                />
-              </a>
-            </div>
-            <div className="flex justify-center">
-              <a
-                href="https://www.voxels.com/play?coords=W@429W,182S"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  className="mx-auto"
-                  src="/bittrees_logo_tree.png"
-                  width="100"
-                  height="100"
-                  alt="Bittrees Research logo"
-                />
-              </a>
-            </div>
-            <div className="w-60">
-              <a
-                href="https://paragraph.xyz/@bittrees_research"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  className="mx-auto"
-                  src="/paragraph-logo.png"
-                  width="100"
-                  height="100"
-                  alt="paragraph.xyz logo"
-                />
-              </a>
-            </div>
-          </footer>
         </div>
       </main>
     </div>
