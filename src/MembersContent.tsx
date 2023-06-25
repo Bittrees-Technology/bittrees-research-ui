7504b447a5b1777e14112d1ebd606d6585637f5c
Brian Gershon <briangershon@users.noreply.github.com>
2023-06-25 12:50:11 -0700

    Create new routes for both mints (membership and upcoming brgov) (#17)
    * Move both mint pages under /mint and redirect current /mint page to /mint/membership
    
    In content, replace /mint links with /mint/membership
    
    Clean-up imports via VSCode's "Organize Imports" and run Prettier.
    
    * Remove BRGOV link since not added yet

--- src/MembersContent.tsx
+++ src/MembersContent.tsx
@@ -1,7 +1,7 @@
 import { useAccount } from "wagmi";
 import { useState, useEffect } from "react";
-import { useCookies } from 'react-cookie';
-import { hasActiveMembership, CONTRACT_ADDRESS } from './alchemy';
+import { useCookies } from "react-cookie";
+import { hasActiveMembership, CONTRACT_ADDRESS } from "./alchemy";
 
 /**
  * MembersContent React component.
@@ -21,20 +21,19 @@ export function MembersContent() {
   useEffect(() => {
     let token = cookies[CONTRACT_ADDRESS];
     if (!token) {
-
       console.log("isConnected: " + isConnected);
       if (!(address && isConnected)) {
         setHasValidMembership(false);
         return;
       }
-  
+
       hasActiveMembership(address)
         .then((hasActiveMembership) => {
           if (hasActiveMembership) {
             setCookie(CONTRACT_ADDRESS, address, {
-              path: '/'
+              path: "/",
             });
-          } 
+          }
           setHasValidMembership(hasActiveMembership);
         })
         .catch((err) => {
@@ -44,7 +43,6 @@ export function MembersContent() {
         .finally(() => {
           setLoading(false);
         });
-
     } else {
       setHasValidMembership(true);
       setLoading(false);
@@ -62,77 +60,103 @@ export function MembersContent() {
 
       {hasValidMembership && (
         <div className="m-4 mx-auto max-w-xl">
-          <h2 className="text-xl font-newtimesroman font-bold">Member Services</h2>
+          <h2 className="text-xl font-newtimesroman font-bold">
+            Member Services
+          </h2>
           <br />
           <div className="grid grid-cols-2 gap-6">
             <div className="text-left font-newtimesroman">
-            <ul className="max-w-md space-y-1 text-gray-400 list-disc list-outside ms-4 dark:text-gray-400 ">
+              <ul className="max-w-md space-y-1 text-gray-400 list-disc list-outside ms-4 dark:text-gray-400 ">
+                <li className="p-2">Gift Membership</li>
                 <li className="p-2">
-                  Gift Membership
-                </li>
-                <li className="p-2">
-                <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
                     rel="noreferrer"
-                    href="https://etherscan.io/token/0xc8121e650bd797d8b9dad00227a9a77ef603a84a#code">Membership Contract</a>
+                    href="https://etherscan.io/token/0xc8121e650bd797d8b9dad00227a9a77ef603a84a#code"
+                  >
+                    Membership Contract
+                  </a>
                 </li>
-                <li className="p-2"><a className="text-black no-underline hover:underline hover:text-green-700" 
-                    href="/mintBRGOV">Mint BRGOV</a></li>
+                <li className="p-2">
+                  {/* <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    href="/mint/brgov"
+                  > */}
+                  Mint BRGOV
+                  {/* </a> */}
+                </li>
                 <li className="p-2">BRGOV Contract </li>
                 <li className="p-2">Mint BRTC</li>
-                <li className="p-2">
-                    Stake BRTC
-                </li>
+                <li className="p-2">Stake BRTC</li>
                 <li className="p-2">Burn BRTC</li>
               </ul>
-
             </div>
             <div className="text-left font-newtimesroman">
-              
-            <ul className="max-w-md space-y-1 text-gray-400 list-disc list-outside ms-4 dark:text-gray-400 ">
-            <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    href="/visionstatement">Vision Statement</a>
+              <ul className="max-w-md space-y-1 text-gray-400 list-disc list-outside ms-4 dark:text-gray-400 ">
+                <li className="p-2">
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    href="/visionstatement"
+                  >
+                    Vision Statement
+                  </a>
                 </li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    href="/codeofethics">Code of Ethics</a>
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    href="/codeofethics"
+                  >
+                    Code of Ethics
+                  </a>
                 </li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
                     rel="noreferrer"
-                    href="https://docs.google.com/document/d/1ncY1zhrYFzpAh9hrSqCmO6z3dftXTfWV1RXHGHrTwlQ/edit">
+                    href="https://docs.google.com/document/d/1ncY1zhrYFzpAh9hrSqCmO6z3dftXTfWV1RXHGHrTwlQ/edit"
+                  >
                     Roadmap
-                  </a>  
+                  </a>
                 </li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
                     rel="noreferrer"
-                    href="https://docs.google.com/drawings/d/1_AYqj8boh7o8d_CrhSbSUtlvrs0fpTOUEIOxqGd_s58/">
+                    href="https://docs.google.com/drawings/d/1_AYqj8boh7o8d_CrhSbSUtlvrs0fpTOUEIOxqGd_s58/"
+                  >
                     Org Chart Diagram
                   </a>
                 </li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
                     rel="noreferrer"
-                    href="https://guild.xyz/bittrees-research">
+                    href="https://guild.xyz/bittrees-research"
+                  >
                     Telegram
                   </a>
                 </li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
                     rel="noreferrer"
-                    href="https://twitter.com/BittreesR">Twitter</a>
+                    href="https://twitter.com/BittreesR"
+                  >
+                    Twitter
+                  </a>
                 </li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
                     rel="noreferrer"
-                    href="https://paragraph.xyz/@bittrees_research">
+                    href="https://paragraph.xyz/@bittrees_research"
+                  >
                     Paragraph
                   </a>
                 </li>
@@ -144,7 +168,7 @@ export function MembersContent() {
 
       {!hasValidMembership && (
         <div className="m-4 mx-auto max-w-xl font-newtimesroman">
-          <a href="/mint">Please go mint your membership!</a>
+          <a href="/mint/membership">Please go mint your membership!</a>
         </div>
       )}
     </>
