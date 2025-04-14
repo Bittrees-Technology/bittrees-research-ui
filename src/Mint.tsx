90baa79e95d1488f3681263680374b60179c96bf
Brian Gershon <briangershon@users.noreply.github.com>
2025-04-13 23:48:09 -0700

    Add styling to button to replace daisyui version

--- src/Mint.tsx
+++ src/Mint.tsx
@@ -80,7 +80,7 @@ export function Mint() {
 
       <div className="mt-4 font-newtimesroman">
         <button
-          className="btn btn-primary"
+          className="inline-flex items-center justify-center px-4 py-2 font-medium text-white bg-blue-600 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
           onClick={onClick}
           disabled={!Boolean(address) || Boolean(error)}
         >
